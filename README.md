3398636fb32d0df9bcf26c67eead8f3c4d659873
Eric Coutinho <ericoutinho@gmail.com>
2021-07-27 20:22:41 -0300

    v1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # number-in-full
-Números por extenso em Vanilla JS
+
+### Escreva números por extenso
+
+Classe simples em Vanilla JS para retornar números por extenso com exibição da moeda opcional.
+
+Utilize o segundo parâmetro como **true**, caso desejar que a moeda seja exibida (R$).
+
+```
+    let n = new NumberIF(number, true);
+    console.log( n.readNumber() );
+```
+
+**Exemplo 1**: Escreva o número **12.500,35** por extenso.
+
+```
+    let n = new NumberIF("12,530.35");
+    console.log( n.readNumber() );
+
+    > doze mil, quinhentos e trinta e trinta e cinco
+```
+
+**Exemplo 2**: Escreva o mesmo número por extenso com exibição da moeda.
+
+```
+    let n = new NumberIF("12,530.35", true);
+    console.log( n.readNumber() );
+
+    > doze mil, quinhentos e trinta reais e trinta e cinco centavos
+```
--- script.js
+++ script.js
@@ -1,90 +1,73 @@
-const units    = ['', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
-const ten      = ['dez', 'onze', 'doze', 'treze', 'quartorze', 'quinze', 'dessseis', 'dezessete', 'dezoito', 'dezenove'];
-const dozens   = ['', '', 'vinte', 'trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa'];
-const hundreds = ['', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos','novecentos'];
+class NumberIF {
 
-const classSingular  = ['', 'mil', 'milhão', 'bilhão', 'trilhão', 'quatrilhão' ];
-const classPlural    = ['', 'mil', 'milhões', 'bilhões', 'trilhões', 'quatrilhões'];
+    units    = ['', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
+    ten      = ['dez', 'onze', 'doze', 'treze', 'quartorze', 'quinze', 'dessseis', 'dezessete', 'dezoito', 'dezenove'];
+    dozens   = ['', '', 'vinte', 'trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa'];
+    hundreds = ['', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos','novecentos'];
 
-/**
- * Faz a leitura do numero por extenso
- * @param {string} number Numero a ser lido por extenso
- * @param {boolean} currency Caso desejar acrescenta a moeda
- * @returns {string}
- */
-function readNumber(number, currency = false) {
-    
-    let dc = number.split(".")[1];
-    let it = number.split(".")[0];
-    let result = [];
-    
-    let parts = it.split(",");
-    
-    for (let i = 0; i <= parts.length - 1; i++ ) {
-         result.push(readPart( parts[i], ((parts.length - 1) - i) ));
+    classSingular  = ['', 'mil', 'milhão', 'bilhão', 'trilhão', 'quatrilhão' ];
+    classPlural    = ['', 'mil', 'milhões', 'bilhões', 'trilhões', 'quatrilhões' ];
+
+    constructor(number, currency = false) {
+        this.number = number;
+        this.currency = currency;
     }
 
-    let integer  = result.join(", ") + (currency ? " reais" : "");
-    let decimals = readDecimals(dc) + (currency ? " centavos" : "");
+    readNumber() {
+        let dc = this.number.split(".")[1];
+        let it = this.number.split(".")[0];
+        let result = [];
+        
+        let parts = it.split(",");
+        
+        for (let i = 0; i <= parts.length - 1; i++ ) {
+            result.push(this.readPart( parts[i], ((parts.length - 1) - i) ));
+        }
 
-    return integer + decimals;
-}
+        let integer  = result.join(", ") + (this.currency ? " reais" : "");
+        
+        let decimals = this.readDecimals(dc) + (this.currency ? " centavos" : "");
 
-/**
- * Faz a leitura do decimal
- * @param {string} decimal Decimal a ser lido por extenso
- * @returns {string}
- */
-function readDecimals(decimal) {
-    return  (parseInt(decimal) > 0) ? ` e ${readPart(decimal)}` : '';
-}
-
-
-/**
- * Lê por extenso a centena da classe informada
- * @param {string} part A centena a ser lida
- * @param {string} classNumber A classe da centena a ser lida
- * @returns {string}
- */
-function readPart(part, classNumber) {
-    let number = padZeros(part);
-    let c='', d='', u='', h='';
-    
-    if ( number[1] === '0' && number[2] === '0') {
-        c = "cem"
-    } else {
-        c = parseInt(number[0]) > 0 ? hundreds[number[0]] : '';
-    }
-    
-    if ( number[1] === '1' ) {
-        d = ten[number[2]];
-        u = '';
-    } else {
-        d = parseInt( number[1] ) > 0 ? dozens[number[1]] : '';
-        u = parseInt( number[2] ) > 0 ? units[number[2]] : '';
+        return integer + decimals;
     }
 
-    if (parseInt(number) > 0) {
-        h = (parseInt(number) > 1) ? classPlural[classNumber] : classSingular[classNumber];
+    readPart(part, classNumber) {
+        let number = this.padZeros(part);
+        let c='', d='', u='', h='';
+        
+        if ( number[0] ==='1' && number[1] === '0' && number[2] === '0') {
+            c = "cem"
+        } else {
+            c = parseInt(number[0]) > 0 ? this.hundreds[number[0]] : '';
+        }
+        
+        if ( number[1] === '1' ) {
+            d = this.ten[number[2]];
+            u = '';
+        } else {
+            d = parseInt( number[1] ) > 0 ? this.dozens[number[1]] : '';
+            u = parseInt( number[2] ) > 0 ? this.units[number[2]] : '';
+        }
+
+        if (parseInt(number) > 0) {
+            h = (parseInt(number) > 1) ? this.classPlural[classNumber] : this.classSingular[classNumber];
+        }
+
+        return c + (((c && d) || (c && u)) ? ' e ' : '') + d + (d && u ? ' e ' : '') + u + (h ? ` ${h}` : '');
     }
 
-    return c + (((c && d) || (c && u)) ? ' e ' : '') + d + (d && u ? ' e ' : '') + u + (h ? ` ${h}` : '');
+    readDecimals(decimal) {
+        return  (parseInt(decimal) > 0) ? ` e ${this.readPart(decimal)}` : '';
+    }
+
+    padZeros(toPadNumber){
+        return (`000${toPadNumber}`).slice(-3);
+    }
 }
 
-
-/**
- * Preencher com zeros a esquerda caso o número
- * seja menor que 3 casas
- * @param {string} number O número a ser ascrescido de zeros
- * @returns {string}
- */
-function padZeros(number) {
-    return (`000${number}`).slice(-3);
-}
-
-
-let number = "1,503,252,125,023.35";
+let number = "111,530.35";
 console.warn(number);
 
 
-console.log(readNumber(number, true));
+let n = new NumberIF(number, true);
+console.log(n.readNumber());
